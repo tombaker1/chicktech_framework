a87d5f61456150bc887187c1b310eb3109da88b8
Tom Baker <thomas.baker@intel.com>
2016-02-19 15:43:17 -0800

    Starting communication, added test button to home screen

--- app/client_framework/www/js/app.js
+++ app/client_framework/www/js/app.js
@@ -48,3 +48,7 @@ document.addEventListener("app.Ready", onAppReady, false) ;
 
 // NOTE: change "dev.LOG" in "init-dev.js" to "true" to enable some console.log
 // messages that can help you debug Cordova app initialization issues.
+
+var app = {
+    communicator: null
+};
